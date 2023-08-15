ef14f20c62be091e665746337cbbf51551bcd8df
Alexis Parave <aparavev@gmail.com>
2023-08-14 19:49:27 -0500

    :sparkles: Add routes for products

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import express from 'express'
+import productsRoutes from './routes/products.routes.js'
 
 const app = express()
+
+app.use('/products', productsRoutes)
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Server listening on port', process.env.PORT || 3000)
