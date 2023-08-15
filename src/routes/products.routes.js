6e434f7e6b8bcaa5a6356068b8ffd5c1f198f72d
Alexis Parave <aparavev@gmail.com>
2023-08-15 18:58:38 -0500

    :sparkles: Add middlaware to authneticate user with jwt

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import * as productController from '../controllers/productController.js'
+import verifyJwt from '../middlewares/verifyJwt.js'
 
 const router = Router()
 
@@ -7,10 +8,10 @@ router.get('/', productController.getAllProducts)
 
 router.get('/:productId', productController.getProduct)
 
-router.post('/', productController.createProduct)
+router.post('/', verifyJwt, productController.createProduct)
 
-router.put('/:productId', productController.updateProduct)
+router.put('/:productId',verifyJwt, productController.updateProduct)
 
-router.delete('/:productId', productController.deleteProduct)
+router.delete('/:productId', verifyJwt, productController.deleteProduct)
 
 export default router
