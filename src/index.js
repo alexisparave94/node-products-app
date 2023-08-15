133715aa571f2b59092dc3ba580bc970fb33318c
Alexis Parave <aparavev@gmail.com>
2023-08-14 22:51:18 -0500

    :sparkles: Add versioning to api

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = express()
 
 app.use(express.json())
 
-app.use('/products', productsRoutes)
+app.use('/api/v1/products', productsRoutes)
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Server listening on port', process.env.PORT || 3000)
