840e412cce8f77cfaca69688897ef2ae03441b33
Alexis Parave <aparavev@gmail.com>
2023-08-15 19:49:52 -0500

    :sparkles: Add functionality to authorized action depending on the roles

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import * as productController from '../controllers/productController.js'
 import verifyJwt from '../middlewares/verifyJwt.js'
+import { isAdmin, isModerator } from '../middlewares/authorizeAction.js'
 
 const router = Router()
 
@@ -8,10 +9,10 @@ router.get('/', productController.getAllProducts)
 
 router.get('/:productId', productController.getProduct)
 
-router.post('/', verifyJwt, productController.createProduct)
+router.post('/', [verifyJwt, isModerator], productController.createProduct)
 
-router.put('/:productId',verifyJwt, productController.updateProduct)
+router.put('/:productId',[verifyJwt, isModerator], productController.updateProduct)
 
-router.delete('/:productId', verifyJwt, productController.deleteProduct)
+router.delete('/:productId', [verifyJwt, isAdmin], productController.deleteProduct)
 
 export default router
