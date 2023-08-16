b08860ef6b2166d8ca4452f58b80dcdebea337af
Alexis Parave <aparavev@gmail.com>
2023-08-15 23:29:26 -0500

    :sparkles: Add middleware to check if the username or email already exist

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 import * as authController from '../controllers/authController.js'
-
+import { checkDucplicateUsernameAndEmail } from '../middlewares/verifySignup.js'
+ 
 const router = Router()
 
-router.post('/signup', authController.signup)
+router.post('/signup', checkDucplicateUsernameAndEmail, authController.signup)
 
 router.post('/signin', authController.signin)
 
