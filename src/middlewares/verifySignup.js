17fcb402039bf9663652fc46a045d1e366f78b67
Alexis Parave <aparavev@gmail.com>
2023-08-15 23:42:28 -0500

    :bug: Fix status response

--- src/middlewares/verifySignup.js
+++ src/middlewares/verifySignup.js
@@ -2,10 +2,10 @@ import User from "../models/User.js"
 
 export const checkDucplicateUsernameAndEmail = async (req, res, next) => {
   const user = await User.findOne({ username: req.body.username })
-  if (user) return res.status(400).json({ message: 'The username already exist' })
+  if (user) return res.status(422).json({ message: 'The username already exist' })
 
   const email = await User.findOne({ email: req.body.email })
-  if (email) return res.status(400).json({ message: 'The email is already exist' })
+  if (email) return res.status(422).json({ message: 'The email is already exist' })
 
   next()
 }
