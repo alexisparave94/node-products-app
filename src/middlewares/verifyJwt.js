a979998fce51d5338bd851ff261b2ffffebbfe52
Alexis Parave <aparavev@gmail.com>
2023-08-23 12:35:33 -0500

    :recycle: Update header authotizarion JWT

--- src/middlewares/verifyJwt.js
+++ src/middlewares/verifyJwt.js
@@ -3,7 +3,7 @@ import secretKey from '../config/secretKey.js'
 import User from '../models/User.js'
 
 export default async (req, res, next) => {
-  const token = req.headers['x-access-token']
+  const token = req.headers['authorization'].split(' ')[1]
 
   if (!token) return res.status(401).json({message: 'Unauthorized'})
 
