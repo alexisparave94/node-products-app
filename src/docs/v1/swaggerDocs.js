9cf6c7e9f34ae48119f532320386d5937cc56449
Alexis Parave <aparavev@gmail.com>
2023-08-23 02:40:05 -0500

    :sparkles: Setup swagger docs

---
+++ src/docs/v1/swaggerDocs.js
@@ -0,0 +1,25 @@
+import swaggerJSDoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: { title: "Products API", version: "0.1.0" },
+  },
+  apis: ["./src/routes/products.routes.js", "./src/models/Product.js"],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+const swaggerDocs = (app, port) => {
+  app.use("/api/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.get("/api/v1/docs.json", (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.send(swaggerSpec);
+  });
+  console.log(
+    `Version 1 Docs are available on http://localhost:${port}/api/v1/docs`
+  );
+};
+
+export default swaggerDocs
