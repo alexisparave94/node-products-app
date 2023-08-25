011425d8c827e9dc9754de96e9f0bb4deea9d0a1
Alexis Parave <aparavev@gmail.com>
2023-08-25 15:09:32 -0500

    :memo: Add doc for auth routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,7 +4,71 @@ import { checkDucplicateUsernameAndEmail } from '../middlewares/verifySignup.js'
  
 const router = Router()
 
+/**
+ * @openapi
+ * /api/v1/signup:
+ *   post:
+ *     tags: 
+ *       - Auth
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 example: Testino
+ *               email:
+ *                 type: string
+ *                 example: test@mail.com
+ *               password:
+ *                 type: string
+ *                 example: password
+ *     responses:
+ *       201:
+ *         description: User created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *                   example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZGMxNzMyYTk1ZGU3YzQ5MWI3NjkwZSIsImlhdCI6MTY5Mjc4MDU4MywiZXhwIjoxNjkyNzgwNzAzfQ.qnKHmQBqG3-aEGelwnlLP93EGMgFsU5oAO8woASwLLk
+ */
+
 router.post('/signup', checkDucplicateUsernameAndEmail, authController.signup)
+
+/**
+ * @openapi
+ * /api/v1/signin:
+ *   post:
+ *     tags: 
+ *       - Auth
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: test@mail.com
+ *               password:
+ *                 type: string
+ *                 example: password
+ *     responses:
+ *       200:
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *                   example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZGMxNzMyYTk1ZGU3YzQ5MWI3NjkwZSIsImlhdCI6MTY5Mjc4MDU4MywiZXhwIjoxNjkyNzgwNzAzfQ.qnKHmQBqG3-aEGelwnlLP93EGMgFsU5oAO8woASwLLk
+ */
 
 router.post('/signin', authController.signin)
 
