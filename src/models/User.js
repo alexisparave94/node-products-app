978613b63946417c804f04d25cb0a8d4b135ceac
Alexis Parave <aparavev@gmail.com>
2023-08-25 16:07:35 -0500

    :memo: Add user schema

--- src/models/User.js
+++ src/models/User.js
@@ -3,6 +3,30 @@ import bcrypt from 'bcrypt'
 
 export const ROLES = ['admin', 'moderator', 'user']
 
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       properties:
+ *         _id:
+ *           type: string
+ *           example: 61dbae02-c147-4e28-863c-db7bd402b2d6
+ *         username:
+ *           type: string
+ *           example: Testino
+ *         email:
+ *           type: string
+ *           example: test@mail.com
+ *         roles:
+ *           type: array
+ *           items:
+ *             type: string
+ */
+
+
+
 const userSchema = new Schema({
   username: { type: String, require: true, unique: true },
   email: { type: String, require: true, unique: true },
