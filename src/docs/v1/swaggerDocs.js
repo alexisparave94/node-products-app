6faf4aa2992962167d9950db3bbe1dd250528921
Alexis Parave <aparavev@gmail.com>
2023-08-25 18:38:48 -0500

    :fire: Remove console message

--- src/docs/v1/swaggerDocs.js
+++ src/docs/v1/swaggerDocs.js
@@ -29,7 +29,6 @@ const swaggerDocs = (app, port) => {
     res.setHeader("Content-Type", "application/json");
     res.send(swaggerSpec);
   });
-  console.log(options)
   console.log(
     `Version 1 Docs are available on http://localhost:${port}/api/v1/docs`
   );
